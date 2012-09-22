bd874b0651b7b51605413b7c5f242d5873a8e8b9
JangHee <skqwkrdk@naver.com>
2012-09-22 23:16:40 +0900

    facebook 연동

--- nPMService/WebContent/js/openID.js
+++ nPMService/WebContent/js/openID.js
@@ -1,11 +1,20 @@
 var flag = false;
 
+function fbEnsureInit(callback) {
+    if(!window.flag) {
+        setTimeout(function() {fbEnsureInit(callback);}, 50);
+    } else {
+        if(callback) {
+            callback();
+        }
+    }
+}
+
 function fbLogin() {
-	if(flag){
-	FB.getLoginStatus(function(response) {
+	fbEnsureInit(function(response) {
 		FB.login(function(response) {
 			if (response.authResponse) {
-
+				fbGetUser();
 			} else {
 
 			}
@@ -13,61 +22,60 @@ function fbLogin() {
 			scope : 'user_birthday,email,user_work_history'
 		});
 	});
-	}
-	else{
-		alert('잠시만 기다려주세요');
-	}
+}
+
+function fbLogout(){
+	fbEnsureInit(function(response) {
+		FB.logout(function(response) {
+			fbGetUser();
+		});
+	});
 }
 
 function fbGetUser() {
-	fbName = '';
-	fbBirth = '';
-	fbEmail = '';
-	fbID = '';
-	fbWork = '';
-	fbSrc = '';
-	FB.api('/me', function(response) {
-		fbName = response.name;
-		fbBirth = response.birthday;
-		fbEmail = response.email;
-		fbID = response.id;
-		var work = response.work;
-		if(work != null){
-			fbWork = work[0].employer.name;
-		}
-		fbSrc = "http://graph.facebook.com/"
+	fbEnsureInit(function(){
+		fbName = '';
+		fbBirth = '';
+		fbEmail = '';
+		fbID = '';
+		fbWork = '';
+		fbSrc = '';
+		FB.api('/me', function(response) {
+			fbName = response.name;
+			fbBirth = response.birthday;
+			fbEmail = response.email;
+			fbID = response.id;
+			var work = response.work;
+			if(work != null){
+				fbWork = work[0].employer.name;
+			}
+			fbSrc = "http://graph.facebook.com/"
 				+ fbID + "/picture?type=large";
-		document.getElementById('photo').src = fbSrc;
-		 document.getElementById('name').innerHTML = fbName;
-		 document.getElementById('birth').innerHTML = fbBirth;
-		 document.getElementById('email').innerHTML = fbEmail;
-		 document.getElementById('work').innerHTML = fbWork;
-		
+			document.getElementById('photo').src = fbSrc;
+			document.getElementById('name').innerHTML = fbName;
+			document.getElementById('birth').innerHTML = fbBirth;
+			document.getElementById('email').innerHTML = fbEmail;
+			document.getElementById('work').innerHTML = fbWork;
+			if(fbEmail != null){
+				document.getElementById('fb_login_button').value = '로그아웃';
+				document.getElementById('fb_login_button').onclick = fbLogout;
+			}
+			else{
+				document.getElementById('fb_login_button').value = '로그인';
+				document.getElementById('fb_login_button').onclick = fbLogin;
+			}
+		});
 	});
 }
 
 function fbIsLogin() {
 	FB.getLoginStatus(function(response) {
 		if (response.status === 'connected') {
-//			alert('connected');
-			// the user is logged in and has authenticated your
-			// app, and response.authResponse supplies
-			// the user's ID, a valid access token, a signed
-			// request, and the time the access token
-			// and signed request each expire
-			fbloginStatus = true;
 //			var uid = response.authResponse.userID;
 //			var accessToken = response.authResponse.accessToken;
 		} else if (response.status === 'not_authorized') {
-			// the user is logged in to Facebook,
-			// but has not authenticated your app
-//			alert('not connected');
-			fbloginStatus = false;
 			location.replace('joinus.html');
 		} else {
-			// the user isn't logged in to Facebook.
-//			alert('not connected');
-			fbloginStatus = false;
 			location.replace('joinus.html');
 		}
 	});
