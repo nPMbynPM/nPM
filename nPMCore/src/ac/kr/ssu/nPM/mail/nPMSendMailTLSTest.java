fc0d71ee69226a8d4cf706fa7770f64db2192d77
Daewoong <doubletaken@naver.com>
2012-10-19 21:31:07 +0900

    Send Mail Test.

---
+++ nPMCore/src/ac/kr/ssu/nPM/mail/nPMSendMailTLSTest.java
@@ -0,0 +1,49 @@
+package ac.kr.ssu.nPM.mail;
+
+import java.util.Properties;
+ 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+public class nPMSendMailTLSTest {
+	
+	public static void main(String[] args) {
+ 
+		final String username = "test@gmail.com";
+		final String password = "xxxxx";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("doubletaken@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("doubletaken@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Added nPM Project..!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
