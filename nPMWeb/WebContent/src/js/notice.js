489ca3c98ffc56edbd8d71bff4c2604dbc84c111
Yoon <heeeee_0624@nate.com>
2012-10-22 21:18:59 +0900

    추가 주석

--- nPMWeb/WebContent/src/js/notice.js
+++ nPMWeb/WebContent/src/js/notice.js
@@ -1,5 +1,5 @@
 ﻿function setheight() {
-    if (parent.document.getElementById("iframe1")) {
+    if (parent.document.getElementById("iframe1")) {// 유동성있는 처리
         parent.document.getElementById("iframe1").height = document.body.scrollHeight;
     }
 }
